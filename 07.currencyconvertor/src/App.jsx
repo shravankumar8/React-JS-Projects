e73c571a3f1fddfdac01c4526391ad990b35bfee
shravankumar8 <kumashravan5@gmail.com>
2023-10-18 23:09:19 +0530

    ecommerce project

--- 07.currencyconvertor/src/App.jsx
+++ 07.currencyconvertor/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
- <></>
+  return(
+    <>
+    </>
   )
 }
-
 export default App
