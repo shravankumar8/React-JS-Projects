bb478e47e293404f6ca04177f6f902d00a453a75
shravankumar8 <kumashravan5@gmail.com>
2023-10-31 23:27:59 +0530

    updated popular tab

--- 08.ecommerce-cart/src/components/Hero/Hero.jsx
+++ 08.ecommerce-cart/src/components/Hero/Hero.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./Hero.css";
 import arrow_icon from "../Assets/arrow.png"
 import hero_image from "../Assets/hero_image.png"
+import hero_image2 from "../Assets/model-png-1976.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
-       
-          <p>New collections <br />for everyone</p>
-       
+
+        <p>
+          New collections <br />
+          for everyone
+        </p>
 
         <div className="hero-latest-button">
           <div>Latest Collection</div>
@@ -18,7 +21,11 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-right">
-        <img src={hero_image} alt="" />
+        <img src={hero_image2} alt="" />
+
+        <div className="rightimage">
+          <img src={hero_image} alt="" />
+        </div>
       </div>
     </div>
   );
