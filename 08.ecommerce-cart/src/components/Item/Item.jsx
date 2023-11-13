3b3c5d3e88a236c8f5ad53e6e0ae28fc6da6a7de
shravankumar8 <kumashravan5@gmail.com>
2023-11-13 21:57:55 +0530

    updated

--- 08.ecommerce-cart/src/components/Item/Item.jsx
+++ 08.ecommerce-cart/src/components/Item/Item.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import {Link }from 'react-router-dom'
 import './Item.css'
 import pic from '../Assets/p1_product_i1.png'
 
 const Item = (props) => {
   return (
     <div className="Item">
-      <link to={`/product/${props.id}`}>
+      <Link to={`/product/${props.id}`}>
         <img src={props.image} alt="" />
-      </link>
+      </Link>
       <p>{props.name}books</p>
       <div className="item-prices">
         <div className="item-price-new">{props.new_price}</div>
