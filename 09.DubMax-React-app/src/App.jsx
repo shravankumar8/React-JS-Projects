552c0750619b7fa69ce65817913966773e5ea72b
shravankumar8 <kumashravan5@gmail.com>
2023-11-26 12:46:56 +0530

    updated

--- 09.DubMax-React-app/src/App.jsx
+++ 09.DubMax-React-app/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
+import {fetchdatafromapi} from '../src/utils/api'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [movieData, setMovieData] = useState();
+   useEffect(()=>{apitesting()},[])
+  const apitesting=()=>{
+    fetchdatafromapi("/movie/popular")
+    .then((res)=>{
+      console.log(res)
+      setMovieData(res)
+    }).catch((err)=>{
+      console.log("error fetching the movie data ",err)
+    })
+  }
+   
 
   return (
-    <>
-    <div class>
-
+    <div>
+      {movieData ? (
+        <div>
+          {movieData.results.map((movie) => (
+            <div key={movie.key}>{(movie.title +" released in "+  movie.release_date)}</div>
+          ))}
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
     </div>
-      
-    </>
-  )
-}
+  );}
 
 export default App
--- 09.DubMax-React-app/src/utils/api.js
+++ 09.DubMax-React-app/src/utils/api.js
@@ -1,3 +1,20 @@
 import axios from "axios";
+// this is base URL
+const BASE_URL = "https://api.themoviedb.org/3/";
+// this is the token we use for authentication
+const TMDB_token = import.meta.env.VITE_APP_TMDB_TOKEN;
+const headers={Authorization:"Bearer " + TMDB_token}
+export const fetchdatafromapi=async(url,params)=>{
+    try{
+        const {data}=await axios.get(BASE_URL+url,{headers,params})
+        console.log(data)
+        return data;
+    }
+    catch(err){
+        console.error(err);
+        return err
 
-const BASE_URL = "";
+    }
+
+}
+
