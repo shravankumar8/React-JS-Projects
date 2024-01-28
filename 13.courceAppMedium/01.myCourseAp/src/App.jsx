5cbc306c83b87db203461cb52689e3643bb53e25
shravankumar8 <kumashravan5@gmail.com>
2024-01-28 19:54:41 +0530

    koncham completed

--- 13.courceAppMedium/01.myCourseAp/src/App.jsx
+++ 13.courceAppMedium/01.myCourseAp/src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import Signup from './components/Signupp'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Appbar from './components/Appbar'
 import Signin from './components/Signin'
+
 function App() {
-  
+
   return (
     <>
       <div className="mainDiv">
-        <Appbar />
         <Router>
+        <Appbar />
           <Routes>
             <Route path="/Signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
--- 13.courceAppMedium/01.myCourseAp/src/components/Appbar.jsx
+++ 13.courceAppMedium/01.myCourseAp/src/components/Appbar.jsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 // import Button from "@mui/material/Button";
 
 function Appbar() {
+ let navigate = useNavigate();
   return (
     <>
       <div
@@ -16,15 +17,26 @@ function Appbar() {
         <div>
           <Typography style={{ color: "white" }}>coursera</Typography>
         </div>
-        <div style={{display:"flex"}}>
-          <div
-            style={{marginRight:"2px"}}
-          >
-            <Button onClick={()=>{  window.location="/signup"
-}} variant="contained">Signup</Button>
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: "2px" }}>
+            <Button
+              onClick={() => {
+                navigate("/signup")
+              }}
+              variant="contained"
+            >
+              Signup
+            </Button>
           </div>
           <div>
-            <Button onClick={()=>{window.location = "/Signin";}} variant="contained">Signin</Button>
+            <Button
+              onClick={() => {
+                navigate("/signin")
+              }}
+              variant="contained"
+            >
+              Signin
+            </Button>
           </div>
         </div>
       </div>
