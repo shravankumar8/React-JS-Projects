0be96c81107e5c4fe762d1734ffb229a02e0fe75
shravankumar8 <kumashravan5@gmail.com>
2024-02-14 19:57:02 +0530

    input validation for signup

--- 13.courceAppMedium/01.myCourseAp/frontend/src/components/Signupp.jsx
+++ 13.courceAppMedium/01.myCourseAp/frontend/src/components/Signupp.jsx
@@ -7,126 +7,122 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import url from "../assets/url";
 import { userEmailState } from "../store/selectors/username";
-import {  useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { userState } from "../store/atom/admin";
-import { z }from 'zod';
+import { z } from "zod";
 
 function Signup() {
-  
-  let navigate =useNavigate()
+  let navigate = useNavigate();
   const setUserState = useSetRecoilState(userState);
-  if(userEmailState){
-    const [username,setusername]=useState("")
-    const [password,setpassword]=useState("")
-    
-return (
-  <>
-    <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          paddingTop: "140px",
-          marginBottom: "10px",
-          color: "white",
-          // text,
-        }}
-      >
-        <Typography
-          style={{
-            fontSize: "20px",
-          }}
-        >
-          Welcome To Coursera Signup Below
-        </Typography>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Card varint={"outlined"} style={{ width: 400, padding: 40 }}>
-          <TextField
-            onChange={(i) => {
-              setusername(i.target.value);
-            }}
-            label="Email"
-            variant="outlined"
-            fullWidth={true}
-          />
-          <br />
-          <br />
-          <TextField
-            onChange={(i) => {
-              setpassword(i.target.value);
-            }}
-            fullWidth={true}
-            label="Password"
-            variant="outlined"
-            type="password"
-          />
-          <br />
-          <br />
-          <Button
-            variant="contained"
-            onClick={() => {
-              let titleInputProps = z.object({
-                username: z.string().min(1),
-                password: z.string().min(6),
-              });
-               const parsedInput = titleInputProps.safeParse({
-                 username,
-                 password,
-               });
-               if(parsedInput.success) {
-              setusername(parsedInput.data.username)
-              setpassword(parsedInput.data.password)
+  if (userEmailState) {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
 
-                 fetch(`${url}/admin/signup`, {
-                   method: "POST",
-                   headers: { "Content-Type": "application/json" },
-                   body: JSON.stringify(
-                    { username, password }
-                    ),
-                 })
-                   .then((res) => {
-                     if (res.statusCode === 200) {
-                     }
-                     return res.json();
-                   })
-                   .then((data) => {
-                     alert(data.message);
-                     if (data.token) {
-                       setUserState({
-                         isLoading: false,
-                         userMail: username,
-                       });
-                       setTimeout(() => {
-                         navigate("/");
-                       }, 500);
-                       localStorage.setItem("JwtToken", data.token);
-   
-                     }
-                   });
-               }
-               else{
-                alert("mari chinna password vaddu bro inka mari email string ye kottu bro  ",parsedInput.error)
-               }
+    return (
+      <>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              paddingTop: "140px",
+              marginBottom: "10px",
+              color: "white",
+              // text,
             }}
           >
-            Signup
-          </Button>
+            <Typography
+              style={{
+                fontSize: "20px",
+              }}
+            >
+              Welcome To Coursera Signup Below
+            </Typography>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Card varint={"outlined"} style={{ width: 400, padding: 40 }}>
+              <TextField
+                onChange={(i) => {
+                  setusername(i.target.value);
+                }}
+                label="Email"
+                variant="outlined"
+                fullWidth={true}
+              />
+              <br />
+              <br />
+              <TextField
+                onChange={(i) => {
+                  setpassword(i.target.value);
+                }}
+                fullWidth={true}
+                label="Password"
+                variant="outlined"
+                type="password"
+              />
+              <br />
+              <br />
+              <Button
+                variant="contained"
+                onClick={() => {
+                  let titleInputProps = z.object({
+                    username: z.string().min(1).email(),
+                    password: z.string().min(6),
+                  });
+                  const parsedInput = titleInputProps.safeParse({
+                    username,
+                    password,
+                  });
+                  if (parsedInput.success) {
+                    setusername(parsedInput.data.username);
+                    setpassword(parsedInput.data.password);
 
-          <br />
-          <br />
-        </Card>
-      </div>
-    </div>
-  </>
-);
+                    fetch(`${url}/admin/signup`, {
+                      method: "POST",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify({ username, password }),
+                    })
+                      .then((res) => {
+                        if (res.statusCode === 200) {
+                        }
+                        return res.json();
+                      })
+                      .then((data) => {
+                        alert(data.message);
+                        if (data.token) {
+                          setUserState({
+                            isLoading: false,
+                            userMail: username,
+                          });
+                          setTimeout(() => {
+                            navigate("/");
+                          }, 500);
+                          localStorage.setItem("JwtToken", data.token);
+                        }
+                      });
+                  } else {
+                    alert(
+                      parsedInput.error.errors[0].message
+                    );
+                  }
+                }}
+              >
+                Signup
+              </Button>
+
+              <br />
+              <br />
+            </Card>
+          </div>
+        </div>
+      </>
+    );
   }
-  
 }
 
 export default Signup;
